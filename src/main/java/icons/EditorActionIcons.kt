d059eba60b575f70f639573d667d00435828ed78
bofa1ex <bofa1exx@gmail.com>
2021-03-16 01:16:53 +0800

    Add: 新增图标(组合/压缩/转义).
    Took 2 hours 23 minutes

---
+++ src/main/java/icons/EditorActionIcons.kt
@@ -0,0 +1,16 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+
+/**
+ * @author bofa1ex
+ * @since  2021/3/15
+ */
+object EditorActionIcons {
+    @JvmField
+    val Composite = IconLoader.getIcon("/icons/composite.svg", javaClass)
+    @JvmField
+    val Escape = IconLoader.getIcon("/icons/escape.svg", javaClass)
+    @JvmField
+    val Compress = IconLoader.getIcon("/icons/compress.svg", javaClass)
+}
