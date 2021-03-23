c9dadbcf80d8be3fc0d4b21e5a0aeae0496c871d
bofa1ex <bofa1exx@gmail.com>
2021-03-23 14:58:40 +0800

    chore: java file convert to kotlin.

--- src/main/java/com/icoder0/groom/action/EditorEscapeAction.kt
+++ src/main/java/com/icoder0/groom/action/EditorEscapeAction.kt
@@ -1,64 +1,54 @@
-package com.icoder0.groom.action;
+package com.icoder0.groom.action
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import org.apache.commons.lang3.StringUtils
 
 /**
  * @author bofa1ex
  * @since 2021/3/13
  */
-public class EditorEscapeAction extends AnAction {
+class EditorEscapeAction : AnAction() {
     /**
      * Replaces the run of text selected by the primary caret with a fixed string.
      *
      * @param e Event related to this action
      */
-    @Override
-    public void actionPerformed(@NotNull final AnActionEvent e) {
+    override fun actionPerformed(e: AnActionEvent) {
         // Get all the required data from data keys
         // Editor and Project were verified in update(), so they are not null.
-        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-        final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
-        final Document document = editor.getDocument();
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val project = e.getRequiredData(CommonDataKeys.PROJECT)
+        val document = editor.document
         // Work off of the primary caret to get the selection info
-        Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
-        int start = primaryCaret.getSelectionStart();
-        int end = primaryCaret.getSelectionEnd();
+        val primaryCaret = editor.caretModel.primaryCaret
+        val start = primaryCaret.selectionStart
+        val end = primaryCaret.selectionEnd
         // Replace the selection with a fixed string.
         // Must do this document change in a write action context.
-        WriteCommandAction.runWriteCommandAction(project, () ->
-                document.replaceString(start, end, StringUtils.replace(document.getText(), "\"", "\\\""))
-        );
+        WriteCommandAction.runWriteCommandAction(project
+        ) { document.replaceString(start, end, StringUtils.replace(document.text, "\"", "\\\"")) }
         // De-select the text range that was just replaced
-        primaryCaret.removeSelection();
+        primaryCaret.removeSelection()
     }
 
     /**
      * Sets visibility and enables this action menu item if:
-     * <ul>
-     *   <li>a project is open</li>
-     *   <li>an editor is active</li>
-     *   <li>some characters are selected</li>
-     * </ul>
+     *
+     *  * a project is open
+     *  * an editor is active
+     *  * some characters are selected
+     *
      *
      * @param e Event related to this action
      */
-    @Override
-    public void update(@NotNull final AnActionEvent e) {
+    override fun update(e: AnActionEvent) {
         // Get required data keys
-        final Project project = e.getProject();
-        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        val project = e.project
+        val editor = e.getData(CommonDataKeys.EDITOR)
         // Set visibility and enable only in case of existing project and editor and if a selection exists
-        e.getPresentation().setEnabledAndVisible(
-                project != null && editor != null && editor.getSelectionModel().hasSelection()
-        );
+        e.presentation.isEnabledAndVisible = project != null && editor != null && editor.selectionModel.hasSelection()
     }
 }
