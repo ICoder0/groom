d7d0d8d06f71003309c27f7857fdc46305e6b874
bofa1ex <bofa1exx@gmail.com>
2021-03-23 15:05:23 +0800

    update: toolWindow#icon
    Took 2 minutes

---
+++ src/main/java/icons/ToolWindowIcons.kt
@@ -0,0 +1,12 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+
+/**
+ * @author bofa1ex
+ * @since  2021/3/23
+ */
+object ToolWindowIcons {
+    @JvmField
+    val Logo = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
+}
