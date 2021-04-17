e601cce9864db921a734d1bb7ff4d045f4581610
bofa1ex <bofa1exx@gmail.com>
2021-04-17 16:04:36 +0800

    merge: toolWindowIcons => groomIcons add: icon(socket/editor/logo32x).
    Took 23 hours 36 minutes

--- src/main/java/icons/GroomIcons.kt
+++ src/main/java/icons/GroomIcons.kt
@@ -6,11 +6,19 @@ import com.intellij.openapi.util.IconLoader
  * @author bofa1ex
  * @since  2021/3/15
  */
-object EditorActionIcons {
+object GroomIcons {
     @JvmField
     val Composite = IconLoader.getIcon("/icons/composite.svg", javaClass)
     @JvmField
     val Escape = IconLoader.getIcon("/icons/escape.svg", javaClass)
     @JvmField
     val Compress = IconLoader.getIcon("/icons/compress.svg", javaClass)
+    @JvmField
+    val Socket = IconLoader.getIcon("/icons/socket.svg", javaClass)
+    @JvmField
+    val Editor = IconLoader.getIcon("/icons/editor.svg", javaClass)
+    @JvmField
+    val Logo = IconLoader.getIcon("/icons/logo.svg", javaClass)
+    @JvmField
+    val Logo32x = IconLoader.getIcon("/icons/logo32x.svg", javaClass)
 }
