15dbd6d44674d73a37dc51fd507cc6aecb83538e
bofa1ex <bofa1exx@gmail.com>
2021-05-18 12:17:13 +0800

    util: notification-portal#utils(info/error)

---
+++ src/main/java/com/icoder0/groom/component/Notification.kt
@@ -0,0 +1,28 @@
+package com.icoder0.groom.component
+
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+import com.sun.istack.Nullable
+
+
+/**
+ * @author bofa1ex
+ * @since  2021/5/17
+ */
+class NotificationManager {
+    companion object{
+        private val NOTIFICATION_GROUP = NotificationGroup("Custom Notification Group", NotificationDisplayType.BALLOON, true)
+
+        fun notifyError(@Nullable project: Project?, content: String?) {
+            NOTIFICATION_GROUP.createNotification(content!!, NotificationType.ERROR)
+                    .notify(project)
+        }
+
+        fun notifyInfo(@Nullable project: Project?, content: String?){
+            NOTIFICATION_GROUP.createNotification(content!!, NotificationType.INFORMATION)
+                    .notify(project)
+        }
+    }
+}
