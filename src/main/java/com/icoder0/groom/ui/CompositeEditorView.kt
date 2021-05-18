952c1de88fddd70b3e4f1d004dbe857f734000cb
bofa1ex <bofa1exx@gmail.com>
2021-05-18 12:17:36 +0800

    main-views:  - composite-editor  - websocket-client-view bofa1ex A minute ago

---
+++ src/main/java/com/icoder0/groom/ui/CompositeEditorView.kt
@@ -0,0 +1,55 @@
+package com.icoder0.groom.ui
+
+import com.icoder0.groom.component.EditorManager
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.ui.layout.applyToComponent
+import com.intellij.ui.layout.panel
+import com.intellij.util.ui.JBUI.Panels.simplePanel
+
+/**
+ * @author bofa1ex
+ * @since  2021/3/12
+ */
+class CompositeEditorView(project: Project, toolWindow: ToolWindowEx) : GroomToolWindowPanel("Editor", project, toolWindow) {
+
+    override fun initUI(): CompositeEditorView {
+        setContent(panel {
+            row {
+                cell(isFullWidth = true) {
+                    editorWrapperPanel(grow, pushY).applyToComponent {
+                        addToCenter(editor.component)
+                    }
+                }
+                right {
+                    languageComboBox().applyToComponent {
+                        addActionListener {
+                            val oldText: String = this@CompositeEditorView.editor.document.text
+                            editorWrapperPanel.remove(this@CompositeEditorView.editor.component)
+                            this@CompositeEditorView.editor = EditorManager.getEditor(this@CompositeEditorView, this.selectedItem as String)
+                            WriteCommandAction.runWriteCommandAction(project) {
+                                this@CompositeEditorView.editor.document.setText(oldText)
+                            }
+                            editorWrapperPanel.add(this@CompositeEditorView.editor.component)
+                            this@CompositeEditorView.editorWrapperPanel.updateUI()
+                        }
+                    }
+                }
+            }
+        })
+        return this
+    }
+
+    override fun dispose() {
+        super.dispose()
+        EditorManager.disposePanel(this)
+    }
+
+    var editor = EditorManager.getEditor(this, "json")
+
+    var languageComboBox = ComboBox(arrayOf("json", "xml", "html", "plainText"))
+
+    var editorWrapperPanel = simplePanel()
+}
